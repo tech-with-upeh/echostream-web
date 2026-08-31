d2d5dea9d0057b1f592f09dd9f9a522b86d9a761
wisdom upeh <67229614+tech-with-upeh@users.noreply.github.com>
2026-08-31 23:12:02 +0100

    Use shared navbar and footer in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Poppins, DM_Sans } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
-const poppins = DM_Sans({ variable: "--font-poppins", subsets: ["latin"], weight: "300"});
+const dmSans = DM_Sans({ variable: "--font-poppins", subsets: ["latin"], weight: "300" });
 
 export const metadata: Metadata = {
   title: "EchoStream — Let your community be heard",
@@ -11,8 +13,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="en" className={`${poppins.variable} h-full antialiased`}>
-      <body className="min-h-full">{children}</body>
+    <html lang="en" className={`${dmSans.variable} h-full antialiased`}>
+      <body className="min-h-full">
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
